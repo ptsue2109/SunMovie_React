77177c80dcf57fe418dd5317893c8df653ac2f33
van hung <hungtv290501@gmail.com>
2022-10-05 10:30:18 +0700

    Page tickit price

--- src/components/client/Header/Navbar.tsx
+++ src/components/client/Header/Navbar.tsx
@@ -1,22 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-type Props = {}
+import React from "react";
+import { Link } from "react-router-dom";
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
     <>
-        <nav>
-            <ul>
-            <li><Link to={`#`}>Trang chủ</Link></li>
-            <li><Link to={`#`}>Phim</Link></li>
-            <li><Link to={`#`}>Mua vé</Link></li>
-            <li><Link to={`#`}>Giá vé</Link></li>
-            <li><Link to={`#`}>Tin tức</Link></li>
-            <li><Link to={`#`}>Hỗ trợ</Link></li>
-            </ul>
-        </nav>
+      <nav>
+        <ul>
+          <li>
+            <Link to={`/`}>Trang chủ</Link>
+          </li>
+          <li>
+            <Link to={`#`}>Phim</Link>
+          </li>
+          <li>
+            <Link to={`#`}>Mua vé</Link>
+          </li>
+          <li>
+            <Link to={`/tickit-price`}>Giá vé</Link>
+          </li>
+          <li>
+            <Link to={`#`}>Tin tức</Link>
+          </li>
+          <li>
+            <Link to={`#`}>Hỗ trợ</Link>
+          </li>
+        </ul>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
