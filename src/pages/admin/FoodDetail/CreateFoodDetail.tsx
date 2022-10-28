5ce9dd9abe0f933a7ee776f93dd132089fdcef85
taquangnghia <nghiatqph13235@fpt.edu.vn>
2022-10-28 08:28:40 +0700

    Create-FoodDetail

--- src/pages/admin/FoodDetail/CreateFoodDetail.tsx
+++ src/pages/admin/FoodDetail/CreateFoodDetail.tsx
@@ -1,9 +1,36 @@
+import { Button, Form, Input } from "antd";
 import React from "react";
 
 type Props = {};
 
 const CreateFoodDetail = (props: Props) => {
-  return <div>CreateFoodDetail</div>;
+  return (
+    <>
+      <Form layout="vertical" autoComplete="off">
+        <Form.Item
+          name="total"
+          label="total"
+          rules={[
+            { required: true, message: "Không được để trống! ", min: 10 },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="extfoodIdraPrice"
+          label="foodId"
+          rules={[{ required: true, message: "Không được để trống! " }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 };
 
 export default CreateFoodDetail;
