aa3474c728037cfe5ba9fd25648587e19c243814
taquangnghia <nghiatqph13235@fpt.edu.vn>
2022-10-30 14:00:21 +0700

    SUNC-133 Edit-FoodDetail

--- src/pages/admin/FoodDetail/UploadFoodDetail.tsx
+++ src/pages/admin/FoodDetail/UploadFoodDetail.tsx
@@ -1,9 +1,36 @@
+import { Button, Form, Input } from "antd";
 import React from "react";
 
 type Props = {};
 
 const UploadFoodDetail = (props: Props) => {
-  return <div>UploadFoodDetail</div>;
+  return (
+    <>
+      <Form layout="vertical">
+        <Form.Item
+          name="total"
+          label="total"
+          rules={[
+            { required: true, message: "Không được để trống! ", min: 10 },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="extfoodIdraPrice"
+          label="foodId"
+          rules={[{ required: true, message: "Không được để trống! " }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 };
 
 export default UploadFoodDetail;
