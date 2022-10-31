b77b1a2f5ec78505b6999f1536d75ee09acfa742
taquangnghia <nghiatqph13235@fpt.edu.vn>
2022-10-31 07:30:49 +0700

    EditFoodDetail

--- src/pages/admin/FoodDetail/UploadFoodDetail.tsx
+++ src/pages/admin/FoodDetail/UploadFoodDetail.tsx
@@ -1,12 +1,45 @@
-import { Button, Form, Input } from "antd";
-import React from "react";
+import { Button, Form, Input, message } from "antd";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import configRoute from "../../../config";
+import { useAppDispatch, useAppSelector } from "../../../redux/hook";
+import { updateFoodDetail } from "../../../redux/slice/FoodDetail";
 
 type Props = {};
 
 const UploadFoodDetail = (props: Props) => {
+  const { id } = useParams();
+  const [form] = Form.useForm();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { foodDetail, isErr, isFetching, isSucess } = useAppSelector(
+    (state) => state.foodDetailReducer
+  );
+  const data = foodDetail?.find((item) => item._id === id);
+
+  useEffect(() => {
+    if (data) {
+      form.setFieldsValue({
+        ...data,
+      });
+    }
+  }, [data]);
+
+  const onFinish = async (item: any) => {
+    item._id = id;
+    dispatch(updateFoodDetail(item))
+      .unwrap()
+      .then(() => {
+        message.success({ content: "Sửa thành công" });
+        navigate(configRoute.routes.AdminFoodDetail);
+      })
+      .catch(() => {
+        message.error({ content: "Thất bại" });
+      });
+  };
   return (
     <>
-      <Form layout="vertical">
+      <Form layout="vertical" form={form} onFinish={onFinish}>
         <Form.Item
           name="total"
           label="total"
