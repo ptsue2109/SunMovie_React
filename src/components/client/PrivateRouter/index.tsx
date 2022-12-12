ea533efb0fa93b4b513e7a0e97da5debba03897d
ptsue2109 <phuongthaotrinh2109@gmail.com>
2022-12-12 21:34:52 +0700

    fix private router

--- src/components/client/PrivateRouter/index.tsx
+++ src/components/client/PrivateRouter/index.tsx
@@ -6,8 +6,8 @@ import { useAppSelector } from "../../../redux/hook";
 import Contact from "../Contact";
 
 type PrivateRouteProps = {
-  children: JSX.Element;
-  acceptRole: number;
+   children: JSX.Element;
+   acceptRole: number;
 };
 /* userInfo
    + status : 
@@ -20,44 +20,29 @@ type PrivateRouteProps = {
 */
 
 const PrivateRoute = ({ children, acceptRole }: PrivateRouteProps) => {
-  const navigate = useNavigate();
-  const { isLogged, currentUser } = useAppSelector(
-    (state) => state.authReducer
-  );
-  const { users } = useAppSelector((state: any) => state.userReducer);
-  const userLogin = users?.find((item: any) => item?._id == currentUser?._id);
-  useEffect(() => {
-    if (isLogged === false) {
-      notification.info({
-        message: "Đăng nhập trước khi thực hiện chức năng này",
-      });
-    } else if (isLogged && userLogin?.status !== 1) {
-      notification.info({
-        message:
-          "Tài khoản của bạn đã bị khóa hoặc chưa được xác thực , hãy liên hệ với quản trị viên !!",
-      });
-      setTimeout(() => {
-        navigate(configRoute.routes.contact);
-      }, 2000);
-    } else if (isLogged && userLogin?.status === 1 && userLogin?.role !== 1) {
-      navigate(configRoute.routes.home);
-      notification.info({
-        message: "Hãy đăng nhập với tư cách quản trị viên !!",
-      });
-    } else {
-      navigate(configRoute.routes.dashboard);
-    }
-  }, []);
+   const navigate = useNavigate();
+   const { isLogged, currentUser } = useAppSelector((state) => state.authReducer);
+   const { users } = useAppSelector((state: any) => state.userReducer);
+   const userLogin = users?.find((item: any) => item?._id == currentUser?._id);
+   useEffect(() => {
+      if (isLogged === false) {
+         notification.info({ message: "Đăng nhập trước khi thực hiện chức năng này" });
+      } else if (isLogged) {
+         if (userLogin?.status == 2 || userLogin?.status == 0) {
+            notification.info({ message: "Tài khoản của bạn đã bị khóa hoặc chưa được xác thực , hãy liên hệ với quản trị viên !!" });
+            navigate(configRoute.routes.contact)
+         } else if (userLogin?.status == 1) {
+            if (userLogin?.role !== 1) {
+               notification.info({ message: "Đăng nhập với tư cách quản trị viên" })
+               navigate(configRoute.routes.signin)
+            } else {
+               navigate(configRoute.routes.dashboard)
+            }
+         }
+      }
+   }, [userLogin]);
 
-  if (!isLogged) {
-    return <Navigate to={configRoute.routes.signin} />;
-  } else if (isLogged && userLogin?.status !== 1) {
-    return <Contact />;
-  } else if (isLogged && userLogin?.status === 1 && userLogin?.role !== 1) {
-    return <Navigate to={configRoute.routes.dashboard} />;
-  } else {
-    return children;
-  }
+   return children;
 };
 
 export default PrivateRoute;
