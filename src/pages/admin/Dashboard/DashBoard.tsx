5bed0596e54b96072ae704363d97509c7935d5f8
van hung <havt99pro@gmail.com>
2022-12-16 20:24:18 +0700

    update dashboard

--- src/pages/admin/Dashboard/DashBoard.tsx
+++ src/pages/admin/Dashboard/DashBoard.tsx
@@ -3,7 +3,7 @@ import { MovieApi } from "../../../service/MovieApi";
 import { orderApi } from "../../../service/orders";
 import { UserApi } from "../../../service/userApi";
 import { RiMovie2Fill, RiUserLine } from "react-icons/ri";
-import {FaMoneyCheckAlt} from "react-icons/fa"
+import { FaMoneyCheckAlt } from "react-icons/fa";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -89,10 +89,12 @@ const Dashboard = (props: Props) => {
           <div className="text-center">
             <span className="block text-black font-semibold">
               {dashboard.length !== 0
-                ? formatCurrency(dashboard?.weekProfit[0]?.weekTotal)
+                ? formatCurrency(dashboard?.dayProfit[0]?.dayTotal)
                 : ""}
             </span>
-            <span className="text-sm font-semibold">Doanh thu tuần</span>
+            <span className="text-sm font-semibold">
+              Doanh thu ngày hôm nay
+            </span>
           </div>
         </div>
         <div className="flex items-center p-3 justify-center bg-white rounded-md text-[#b5b5c3]">
