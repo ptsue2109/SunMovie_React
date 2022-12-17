d7372754509ff8bd20754dfe02fa7bfd067d7dce
van hung <hungtv290501@gmail.com>
2022-12-17 12:40:58 +0700

    update info movie in list movie

--- src/pages/admin/Movie/ListMovie.tsx
+++ src/pages/admin/Movie/ListMovie.tsx
@@ -1,12 +1,17 @@
-import { Button, message, Popconfirm, Space, Table } from "antd";
+import { Button, message, Modal, Popconfirm, Space, Table } from "antd";
 import { useAppDispatch, useAppSelector } from "../../../redux/hook";
 import { Link } from "react-router-dom";
 import { removeMovieItem } from "../../../redux/slice/Movie";
 import DataTable from "../../../components/admin/Form&Table/Table";
 import { DeleteOutlined, EditOutlined, PlusOutlined } from "@ant-design/icons";
-import { formatDate, convertMovieTime } from "../../../ultils";
+import {
+  formatDate,
+  convertMovieTime,
+  formatDateString,
+  formatCurrency,
+} from "../../../ultils";
 import configRoute from "../../../config";
-
+import { AiOutlineInfoCircle } from "react-icons/ai";
 type Props = {};
 
 const ListMovie = (props: Props) => {
@@ -42,13 +47,13 @@ const ListMovie = (props: Props) => {
       render: (_: any, { name, _id }: any) => <Link to={_id}>{name}</Link>,
     },
 
-    {
-      title: "Thời gian chiếu",
-      dataIndex: "runTime",
-      render: (_: any, record: any) => (
-        <p>{convertMovieTime(record?.runTime)}</p>
-      ),
-    },
+    // {
+    //   title: "Thời gian chiếu",
+    //   dataIndex: "runTime",
+    //   render: (_: any, record: any) => (
+    //     <p>{convertMovieTime(record?.runTime)}</p>
+    //   ),
+    // },
 
     // {
     //   title: "Độ tuổi",
@@ -69,16 +74,33 @@ const ListMovie = (props: Props) => {
       ),
     },
     {
+      title: "Doanh thu",
+      key: "profit",
+      render: (_: any, record: any) => (
+        <div>
+          <p>{formatCurrency(record?.profit)}</p>
+        </div>
+      ),
+    },
+    {
       title: "Hành động",
       key: "action",
       fixed: "right",
-      render: (_: any, record: any) => (
+      render: (item: any, record: any) => (
         <Space size="middle">
           <Link to={`${record._id}`}>
             <EditOutlined
               style={{ color: "var(--primary)", fontSize: "18px" }}
             />
           </Link>
+          <AiOutlineInfoCircle
+            onClick={() => info(item._id)}
+            style={{
+              color: "var(--primary)",
+              fontSize: "18px,",
+              cursor: "pointer",
+            }}
+          />
           {/* <Popconfirm
             title={`Delete ${record?.name ?? record?._id}?`}
             okText="OK"
@@ -126,9 +148,44 @@ const ListMovie = (props: Props) => {
       director: item?.director,
       description: item?.description,
       status: item?.status,
+      profit: item?.profit,
     };
   });
+  const info = (id: any) => {
+    const movieOne = movie.find((item: any) => item._id === id);
 
+    Modal.info({
+      title: `${movieOne?.name} - ( Doanh thu: ${formatCurrency(
+        movieOne?.profit
+      )})`,
+      width: 1000,
+      content: (
+        <div>
+          <div className="grid grid-cols-[30%,70%]">
+            <div>
+              <img src={movieOne?.image[0]?.url} alt="" width={200} />
+            </div>
+            <div>
+              <p></p>
+              <p>
+                Loại phim:{" "}
+                {movieOne?.movieTypeId?.map(
+                  (item: any) => item.movieName + ", "
+                )}
+              </p>
+              <p>Thời lượng: {convertMovieTime(movieOne?.runTime)}</p>
+              <p>Diễn viên: {movieOne?.actor}</p>
+              <p>Đạo diễn: {movieOne?.director}</p>
+              <p>Quốc gia: {movieOne?.country}</p>
+              <p>Ngày khởi chiếu: {formatDateString(movieOne?.releaseDate)}</p>
+            </div>
+          </div>
+          <div className="mt-4">{movieOne?.description}</div>
+        </div>
+      ),
+      onOk() {},
+    });
+  };
   return (
     <div>
       <div className="flex gap-5">
